807be39c277eef4fa75dc3fd1c994374bd532484
joelderbyshiredigital <info@joelderbyshire.digital>
2020-05-08 18:14:29 +0100

    passport setup

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('login', 'Auth\APIAuthController@login');
+Route::post('register', 'Auth\APIAuthController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'Auth\APIAuthController@details');
+});
